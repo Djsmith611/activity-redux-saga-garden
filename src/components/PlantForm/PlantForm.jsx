5f75cf564bbe72b2aeb6fc02307c8745e95e429c
David Smith <djsmith611@me.com>
2024-05-03 15:21:10 -0700

    added inputs to plantform

--- src/components/PlantForm/PlantForm.jsx
+++ src/components/PlantForm/PlantForm.jsx
@@ -1,34 +1,96 @@
-import React, { useState }from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 
 const PlantForm = () => {
-    const dispatch = useDispatch();
-    
-    //Initial state is an OBJECT, with keys id and name
-    let [newPlant, setPlant] = useState({id: 4, name: ''});
+  const dispatch = useDispatch();
 
-    const handleNameChange = (event) => {
-        //Similar to in redux -- we dont want to get rid of the id field when we update name
-        setPlant({...newPlant, name: event.target.value})
+  //Initial state is an OBJECT, with keys id and name
+  let [newPlant, setPlant] = useState({
+    name: "",
+    kingdom: "",
+    clade: "",
+    order: "",
+    family: "",
+    subfamily: "",
+    genus: "",
+  });
+
+  const handleChange = (event, valueToChange) => {
+    switch (valueToChange) {
+      case "name":
+        setPlant({ ...newPlant, name: event.target.value });
+      case "kingdom":
+        setPlant({ ...newPlant, kingdom: event.target.value });
+      case "clade":
+        setPlant({ ...newPlant, clade: event.target.value });
+      case "order":
+        setPlant({ ...newPlant, order: event.target.value });
+      case "family":
+        setPlant({ ...newPlant, family: event.target.value });
+      case "subfamily":
+        setPlant({ ...newPlant, subfamily: event.target.value });
+      case "genus":
+        setPlant({ ...newPlant, genus: event.target.value });
     }
+  };
 
-    const addNewPlant = event => {
-        event.preventDefault();
-        dispatch({ type: 'ADD_PLANT', payload: newPlant });
-        //updates the next plant to have a new id
-        setPlant({id:newPlant.id + 1, name: ''});
-    }
-    return (
-        <div>
-            <h3>This is the form</h3>
-            <pre>{JSON.stringify(newPlant)}</pre>
-            <form onSubmit={addNewPlant}>
-                <input type='text' value={newPlant.name} onChange={handleNameChange} />
-                <input type='submit' value='Add New Plant' />
-            </form>
-        </div>
-    );
-}
-
+  const addNewPlant = (event) => {
+    event.preventDefault();
+    dispatch({ type: "ADD_PLANT", payload: newPlant });
+    setPlant({
+      name: "",
+      kingdom: "",
+      clade: "",
+      order: "",
+      family: "",
+      subfamily: "",
+      genus: "",
+    });
+  };
+  return (
+    <div>
+      <h3>This is the form</h3>
+      <pre>{JSON.stringify(newPlant)}</pre>
+      <form onSubmit={addNewPlant}>
+        <input
+          type="text"
+          value={newPlant.name}
+          onChange={() => handleChange("name")}
+        />
+        <input
+          type="text"
+          value={newPlant.kingdom}
+          onChange={() => handleChange("kingdom")}
+        />
+        <input
+          type="text"
+          value={newPlant.clade}
+          onChange={() => handleChange("clade")}
+        />
+        <input
+          type="text"
+          value={newPlant.order}
+          onChange={() => handleChange("order")}
+        />
+        <input
+          type="text"
+          value={newPlant.family}
+          onChange={() => handleChange("family")}
+        />
+        <input
+          type="text"
+          value={newPlant.subfamily}
+          onChange={() => handleChange("subfamily")}
+        />
+        <input
+          type="text"
+          value={newPlant.genus}
+          onChange={() => handleChange("genus")}
+        />
+        <input type="submit" value="Add New Plant" />
+      </form>
+    </div>
+  );
+};
 
 export default PlantForm;
